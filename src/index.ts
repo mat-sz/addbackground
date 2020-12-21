5bb8aeebc7198b97dc880f8302dd0d4fda3ec4c2
Mat Sz <contact@matsz.dev>
2020-12-21 15:11:31 +0100

    added animation controls

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,19 @@ export interface BackgroundOptions {
   backgroundColor?: string;
 }
 
+export interface BackgroundControls {
+  isPlaying: boolean;
+  setIsPlaying: (value: boolean) => void;
+  stop: () => void;
+}
+
 export function addBackground({
   canvas,
   type,
   primaryColor = 'rgba(255, 255, 255, 0.05)',
   secondaryColor = 'rgba(0, 0, 0, 0.05)',
   backgroundColor = 'transparent',
-}: BackgroundOptions): void {
+}: BackgroundOptions): BackgroundControls {
   if (!(type in backgrounds)) {
     throw new Error('Unsupported background type.');
   }
@@ -32,17 +38,47 @@ export function addBackground({
     backgroundColor,
   });
 
+  let playing = true;
+  let stopped = false;
+
   const frame = () => {
-    if (backgroundColor === 'transparent') {
+    if (stopped) {
+      ctx.fillStyle = 'transparent';
+      ctx.globalAlpha = 1;
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-    } else {
-      ctx.fillStyle = backgroundColor;
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      return;
     }
 
-    renderFunction();
+    if (playing) {
+      if (backgroundColor === 'transparent') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      } else {
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+      }
+
+      renderFunction();
+    }
+
     requestAnimationFrame(frame);
   };
 
   requestAnimationFrame(frame);
+
+  return {
+    get isPlaying() {
+      return playing;
+    },
+    setIsPlaying: value => {
+      if (stopped) {
+        throw new Error('Animation was stopped.');
+      }
+
+      playing = value;
+    },
+    stop: () => {
+      playing = false;
+      stopped = true;
+    },
+  };
 }
